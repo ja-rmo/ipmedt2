796f591782f3a349861d97c9c19d1c422ba7d0f6
Gijs Buis <gijs.buis@me.com>
2020-11-02 20:51:37 +0100

    Added icons to Congo map, made some clickable, added text. Also added kinshasa.html as page for explanation about Kinshasa.

---
+++ js/congoMap.js
@@ -0,0 +1,6 @@
+let clickableMinistry = document.getElementById("js--clickable-ministry");
+
+clickableMinistry.onclick = function() {
+    console.log("Hello there");
+    window.location.href = "kinshasa.html";
+};
