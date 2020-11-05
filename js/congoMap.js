39d9e33a577df42d56dfcd9ca22c4ba62f296349
Gijs Buis <gijs.buis@me.com>
2020-11-05 22:17:59 +0100

    Added Goma page, made storyline clearer

--- js/congoMap.js
+++ js/congoMap.js
@@ -1,6 +1,5 @@
 let clickableMinistry = document.getElementById("js--clickable-ministry");
 
 clickableMinistry.onclick = function() {
-    console.log("Hello there");
     window.location.href = "kinshasa.html";
 };
